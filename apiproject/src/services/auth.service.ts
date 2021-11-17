fd6da94ac080990d44ab10d912ba2c7b17403a2a
LuigiHer <hernandez4389562@gmail.com>
2021-11-17 06:47:44 -0500

    jwt auth login

--- apiproject/src/services/auth.service.ts
+++ apiproject/src/services/auth.service.ts
@@ -1,10 +1,19 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
 const generator = require("password-generator");
 const cryptoJS = require("crypto-js");
+import {config} from '../config/config';
+import {User} from '../models';
+const jwt = require('jsonwebtoken');
+import {UserRepository} from '../repositories';
+import {repository} from '@loopback/repository';
 
-@injectable({scope: BindingScope.TRANSIENT})
+@injectable({scope: BindingScope.TRANSIENT}
+  )
 export class AuthService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */
+    @repository(UserRepository)
+  public userRepository: UserRepository
+    ) {}
 
   /*
    * Add service methods here
@@ -18,5 +27,42 @@ export class AuthService {
     let claveCifrada = cryptoJS.MD5(clave).toString();
     return claveCifrada;
   }
+
+  //JWT
+  GenerarTokenJWT(User: User) {
+    let token = jwt.sign({
+      data: {
+        id: User.Id,
+        correo: User.email,
+        nombre: User.Name + " " + User.lastName
+      }
+    }, config.claveJWT)
+ 
+    return token
+  }
+
+  validarTokenJWT(token: string) {
+    try {
+      let datos = jwt.verify(token, config.claveJWT);
+      return datos;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  //Autenticacion
+  IdentificarPersona(email: string, password: string) {
+    try {
+      let p = this.userRepository.findOne({where: {email: email, password: password}})
+      if (p) {
+        return p;
+      }
+      return false;
+    } catch {
+      return false;
+    }
+  }
+
+
 
 }
